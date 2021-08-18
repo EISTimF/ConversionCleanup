2700694a78ba9a40849187337a620f424cb96b60
Tim Fagan <tfagan@eisdata.tech>
2021-08-18 11:04:39 -0400

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txt_orig = New System.Windows.Forms.TextBox()
+        Me.txt_cleaned = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txt_orig
+        '
+        Me.txt_orig.AcceptsReturn = True
+        Me.txt_orig.AcceptsTab = True
+        Me.txt_orig.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.txt_orig.BackColor = System.Drawing.Color.White
+        Me.txt_orig.Location = New System.Drawing.Point(12, 36)
+        Me.txt_orig.MaxLength = 0
+        Me.txt_orig.Multiline = True
+        Me.txt_orig.Name = "txt_orig"
+        Me.txt_orig.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txt_orig.Size = New System.Drawing.Size(889, 741)
+        Me.txt_orig.TabIndex = 0
+        '
+        'txt_cleaned
+        '
+        Me.txt_cleaned.AcceptsReturn = True
+        Me.txt_cleaned.AcceptsTab = True
+        Me.txt_cleaned.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txt_cleaned.BackColor = System.Drawing.Color.White
+        Me.txt_cleaned.Location = New System.Drawing.Point(901, 36)
+        Me.txt_cleaned.MaxLength = 0
+        Me.txt_cleaned.Multiline = True
+        Me.txt_cleaned.Name = "txt_cleaned"
+        Me.txt_cleaned.ReadOnly = True
+        Me.txt_cleaned.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txt_cleaned.Size = New System.Drawing.Size(889, 741)
+        Me.txt_cleaned.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = System.Windows.Forms.AnchorStyles.Top
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(407, 11)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(85, 13)
+        Me.Label1.TabIndex = 500
+        Me.Label1.Text = "Paste code here"
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.Button1.Location = New System.Drawing.Point(780, 786)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Cleanup Code"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.Button2.Location = New System.Drawing.Point(921, 786)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(151, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Copy Cleaned Up Code"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1802, 821)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txt_cleaned)
+        Me.Controls.Add(Me.txt_orig)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "ConversionCleanup"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txt_orig As TextBox
+    Friend WithEvents txt_cleaned As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,78 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim cleanedUp As String
+
+        cleanedUp = txt_orig.Text
+
+        cleanedUp = cleanedUp.Replace(".ToString ", ".ToString() ")
+        cleanedUp = cleanedUp.Replace(".ToString,", ".ToString(),")
+        cleanedUp = cleanedUp.Replace(".ToString)", ".ToString())")
+        cleanedUp = cleanedUp.Replace(".ToString;", ".ToString();")
+        cleanedUp = cleanedUp.Replace(".Tables(p_x)", ".Tables[p_x]")
+        cleanedUp = cleanedUp.Replace(".Tables(p_y)", ".Tables[p_y]")
+        cleanedUp = cleanedUp.Replace(".Tables(p_z)", ".Tables[p_z]")
+        cleanedUp = cleanedUp.Replace(".Rows(p_x)", ".Rows[p_x]")
+        cleanedUp = cleanedUp.Replace(".Rows(p_y)", ".Rows[p_y]")
+        cleanedUp = cleanedUp.Replace(".Rows(p_z)", ".Rows[p_z]")
+        cleanedUp = cleanedUp.Replace(".Item(p_x)", ".Item[p_x]")
+        cleanedUp = cleanedUp.Replace(".Item(p_y)", ".Item[p_y]")
+        cleanedUp = cleanedUp.Replace(".Item(p_z)", ".Item[p_z]")
+        cleanedUp = cleanedUp.Replace(""" + """, Environment.NewLine)
+        cleanedUp = cleanedUp.Replace("return_parsed_single_quote_string", "ReturnParsedSingleQuoteString")
+        cleanedUp = cleanedUp.Replace("dba_format_unique_identifier_value", "FormatUID")
+        cleanedUp = cleanedUp.Replace("g_framework.Execute_Query(p_sql)", "ExecuteQuery(p_sql, EisGlobals.EisGeneral.DbName)")
+        cleanedUp = cleanedUp.Replace("g_framework.Get_Data(p_sql)", "GetData(p_sql, EisGlobals.EisGeneral.DbName)")
+        cleanedUp = cleanedUp.Replace("Execute_Query(p_sql)", "ExecuteQuery(p_sql, EisGlobals.EisGeneral.DbName)")
+        cleanedUp = cleanedUp.Replace("Get_Data(p_sql)", "GetData(p_sql, EisGlobals.EisGeneral.DbName)")
+        cleanedUp = cleanedUp.Replace("g_framework.Execute_Query(sql)", "ExecuteQuery(sql, EisGlobals.EisGeneral.DbName)")
+        cleanedUp = cleanedUp.Replace("g_framework.Get_Data(sql)", "GetData(sql, EisGlobals.EisGeneral.DbName)")
+        cleanedUp = cleanedUp.Replace("Execute_Query(sql)", "ExecuteQuery(sql, EisGlobals.EisGeneral.DbName)")
+        cleanedUp = cleanedUp.Replace("Get_Data(sql)", "GetData(sql, EisGlobals.EisGeneral.DbName)")
+        cleanedUp = cleanedUp.Replace("g_framework.Execute_Query", "ExecuteQuery")
+        cleanedUp = cleanedUp.Replace("g_framework.Get_Data", "GetData")
+        cleanedUp = cleanedUp.Replace("Execute_Query", "ExecuteQuery")
+        cleanedUp = cleanedUp.Replace("Get_Data", "GetData")
+        cleanedUp = cleanedUp.Replace("CenterMsgBox", "MessageBox")
+        cleanedUp = cleanedUp.Replace("MsgBoxStyle.Information", "MessageBoxButtons.OK, MessageBoxIcon.Information")
+        cleanedUp = cleanedUp.Replace("MsgBoxStyle.Critical", "MessageBoxButtons.OK, MessageBoxIcon.Error")
+        cleanedUp = cleanedUp.Replace("MsgBoxStyle.YesNo", "MessageBoxButtons.YesNo, MessageBoxIcon.Question")
+        cleanedUp = cleanedUp.Replace("Constants.vbNo", "DialogResult.No")
+        cleanedUp = cleanedUp.Replace("Constants.vbYes", "DialogResult.Yes")
+        cleanedUp = cleanedUp.Replace(" | ", " || ")
+        cleanedUp = cleanedUp.Replace(" & ", " && ")
+        cleanedUp = cleanedUp.Replace("g_user_info.user_id", "Application.Session.EisUserID")
+        cleanedUp = cleanedUp.Replace("string p_sql = """"", "StringBuilder sb = new StringBuilder()")
+        cleanedUp = cleanedUp.Replace("string p_sql", "StringBuilder sb = new StringBuilder()")
+        cleanedUp = cleanedUp.Replace("p_sql = ", "sb.Append(")
+        cleanedUp = cleanedUp.Replace("p_sql &= ", "sb.Append(")
+        cleanedUp = cleanedUp.Replace("p_sql += ", "sb.Append(")
+        cleanedUp = cleanedUp.Replace("return p_sql;", "return sb.ToString();")
+        cleanedUp = cleanedUp.Replace("p_sql,", "sb.ToString(),")
+        cleanedUp = cleanedUp.Replace("string sql = """"", "StringBuilder sb = new StringBuilder()")
+        cleanedUp = cleanedUp.Replace("string sql", "StringBuilder sb = new StringBuilder()")
+        cleanedUp = cleanedUp.Replace("sql = ", "sb.Append(")
+        cleanedUp = cleanedUp.Replace("sql &= ", "sb.Append(")
+        cleanedUp = cleanedUp.Replace("sql += ", "sb.Append(")
+        cleanedUp = cleanedUp.Replace("return sql;", "return sb.ToString();")
+        cleanedUp = cleanedUp.Replace("dba_user_tbl", "eis_group_tbl")
+        cleanedUp = cleanedUp.Replace("dba_id", "eis_group_id")
+        cleanedUp = cleanedUp.Replace("DateAndTime", "DateTime")
+        cleanedUp = cleanedUp.Replace("Constants.VbCrLf", """\n""")
+        cleanedUp = cleanedUp.Replace("sender.cancel = True", "TLB_main.CancelEvent = true;")
+        cleanedUp = cleanedUp.Replace(".TextBox_Properties", "")
+        cleanedUp = cleanedUp.Replace(".DateBox_Properties", "")
+
+        For x As Integer = 0 To 99
+            cleanedUp = cleanedUp.Replace($".Tables({x})", $".Tables[{x}]")
+            cleanedUp = cleanedUp.Replace($".Rows({x})", $".Rows[{x}]")
+            cleanedUp = cleanedUp.Replace($".Item({x})", $"[{x}]")
+            cleanedUp = cleanedUp.Replace($"({x})", $"[{x}]")
+        Next
+
+        txt_cleaned.Text = cleanedUp
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Clipboard.SetText(txt_cleaned.Text)
+    End Sub
+End Class
